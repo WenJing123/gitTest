4487831f433a16b1e3311511afbd0488c314e7c5
yuhang <hehexiaohang@vip.qq.com>
2015-08-27 10:16:18 +0800

    fff

---
+++ gitTest/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  gitTest
+//
+//  Created by yuhang on 15/8/27.
+//  Copyright (c) 2015年 yuhang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+        ff
+    }
+    return 0;
+}
